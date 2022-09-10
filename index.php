70f075d8f2e66dae4acae8859f930f9301a6856c
Acem31 <aymeric.blot@sfr.fr>
2022-09-10 13:31:00 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="fr">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Tableau de bord des capteurs</title>
+    <link rel="stylesheet" href="capteurs.css">
+
+</style><meta name="author" content="BLOT Aymeric">
+  </head>
+  <body>
+    <div id="doc3" class="yui-t1">
+      <div id="menu">
+        <ul>
+          <li><a href="./index.php" title="Valeurs en direct" target="_self">Direct</a></li>
+          <li><a href="./archives.php" title="Archives des capteurs" target="_self">Archives</a></li>
+        </ul>
+      </div>
+      <div class="zone">
+        <div class="capt">
+          Capt1
+        </div>
+        <div class="capt">
+          Capt2
+        </div>
+        <div class="capt">
+          Capt3
+        </div>
+        <div class="capt">
+          Capt4
+        </div>
+      </div>
+      <div class="zone">
+        <div id="hygro">
+          Capt Hygro
+        </div>
+        <div id="reset">
+          Reset données
+        </div>
+      </div>
+    </div>
+	  <footer>
+		  <center>
+			  <p>Copyright 
+    <?php echo date("Y"); ?> 
+mc-informatique.com. Tous droits réservés.
+			  </p>
+		  </center>
+	  </footer>
+  </body>
+</html>
