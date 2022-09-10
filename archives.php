1d47534dd233e20bd4108631ab7ad9f67a8b12c4
Acem31 <aymeric.blot@sfr.fr>
2022-09-10 13:49:59 +0200

    Create archives.php

---
+++ archives.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="fr">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Archives des capteurs</title>
+    <link rel="stylesheet" href="capteurs.css">
+	<link rel="icon" href="favicon.ico" />
+</style><meta name="author" content="BLOT Aymeric">
+  </head>
+  <body>
+    <div id="doc3" class="yui-t1">
+      <div id="menu">
+        <ul>
+          <li><a href="./index.php" title="Valeurs en direct" target="_self">Direct</a></li>
+          <li><a href="./archives.php" title="Archives des capteurs" target="_self">Archives</a></li>
+        </ul>
+      </div>
+      
+    </div>
+	  <footer>
+		  <center>
+			  <p>Copyright 
+    <?php echo date("Y"); ?> 
+<a href="https://www.mc-informatique.fr/" title="Site internet de MC Informatique à l'Union (31)">mc-informatique.fr</a>. Tous droits réservés.
+			  </p>
+		  </center>
+	  </footer>
+  </body>
+</html>
