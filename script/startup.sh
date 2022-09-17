8f402957723aebf4a6f3e260f4e2e93082d2282e
Acem31 <aymeric.blot@sfr.fr>
2022-09-17 13:41:30 +0200

    Update startup.sh

--- script/startup.sh
+++ script/startup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 apt update
-echo temperature | passwd root 
+apt install -y install python3-pip
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+pip3 install docker-compose
+usermod -aG docker capteurs
+wget https://raw.githubusercontent.com/Acem31/capteurs_mcinfo/main/docker/Dockerfile
+wget https://raw.githubusercontent.com/Acem31/capteurs_mcinfo/main/docker/docker-compose.yml
+wget https://raw.githubusercontent.com/Acem31/capteurs_mcinfo/main/python/capteurs_temp_test.py
+service ssh restart
+reboot
