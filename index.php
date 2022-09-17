6293a8383859f36a913ee3703047ff3529d70052
Acem31 <aymeric.blot@sfr.fr>
2022-09-17 14:44:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -28,12 +28,6 @@
 		}
 		$mysqli->close();
 		?></p>
-    <p><?php
-
-$mysqli = new mysqli("mysql_db", "root", "couchpass31", "couch_DB");
-
-echo $mysqli->host_info . "\n";
-?></p>
         </div>
         <div class="capt">
           <h2>Capteur 2</h2>
