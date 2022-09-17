69b8996a39c0449499df99129a393c669444f81a
Acem31 <aymeric.blot@sfr.fr>
2022-09-17 10:45:39 +0200

    Update capteurs_temp_test.py

--- python/capteurs_temp_test.py
+++ python/capteurs_temp_test.py
@@ -1,8 +1,14 @@
 import glob
-import pymysql
+import mysql.connector
 from datetime import datetime
 
-
+def mysqlconnect():
+	conn = mysql.connector.connect(
+		host='localhost',
+		user='couch_user',
+		password = "couchpass",
+		database='couch_DB',
+		)
 
 def lire_fichier (emplacement) :
     fichier = open(emplacement)
@@ -16,18 +22,22 @@ def extraire_temperature (contenu) :
     donnees_temperature = seconde_ligne.split(" ")[9]
     return float(donnees_temperature[2:]) / 1000
 
+mycursor = conn.cursor()
 
+mycursor.execute("CREATE TABLE capteur1 (temperature VARCHAR(255), horodatage VARCHAR(255))")
 routes_capteurs = glob.glob("/sys/bus/w1/devices/28*/w1_slave")
 horodatage = datetime.now()
 horodatage_strg = horodatage.strftime("%X")
-
-
 
 if len(routes_capteurs) > 0 :
     contenu_fichier = lire_fichier(routes_capteurs[0])
     temperature = extraire_temperature(contenu_fichier)
     print ("Temperature :", temperature, "°")
     print ("Heure :", horodatage_strg)
+
+sql = "INSERT INTO capteur1 (temperature, horodatage) VALUES (%s, %s)"
+val = (temperature, horodatage_strg)
+mycursor.execute(sql, val)
 
 else :
     print("Sonde non détectee. Vérifier le branchement, ou rendez-vous dans la section montrant une solution possible")
