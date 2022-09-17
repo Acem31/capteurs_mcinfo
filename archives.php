9f53e3d6bd03c6a612d762d79f454fdb2b67dc1c
Acem31 <aymeric.blot@sfr.fr>
2022-09-17 17:46:45 +0200

    Update archives.php

--- archives.php
+++ archives.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="capteurs.css">
 	<link rel="icon" href="favicon.ico" />
 </style><meta name="author" content="BLOT Aymeric">
+<meta http-equiv="refresh" content="1800">
   </head>
   <body>
     <div id="doc3" class="yui-t1">
