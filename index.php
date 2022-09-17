e71d19c299b08b0d5f0cd885d5a22ee01e5cc18e
Acem31 <aymeric.blot@sfr.fr>
2022-09-17 11:56:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,8 +24,8 @@
 		$requete = "SELECT * FROM capteur1";
 		$resultat = $mysqli->query($requete);
 		while ($ligne = $resultat->fetch_assoc()) {
-			echo $ligne['temperature'];
-		}
+			echo $ligne['temperature']
+		};
 		$mysqli->close();
 		?></p>
         </div>
