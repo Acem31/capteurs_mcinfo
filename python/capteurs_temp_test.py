95cc9436ad4d0b4d813d7222829e988a7b85c9e3
Acem31 <aymeric.blot@sfr.fr>
2022-09-17 18:07:22 +0200

    Update capteurs_temp_test.py

--- python/capteurs_temp_test.py
+++ python/capteurs_temp_test.py
@@ -24,7 +24,7 @@ def extraire_temperature (contenu) :
 
 mycursor = conn.cursor()
 
-mycursor.execute("CREATE TABLE IF NOT EXISTS capteur1 (id INT AUTO_INCREMENT PRIMARY KEY, temperature VARCHAR(255), horodatage VARCHAR(255))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS capteur1 (id INT AUTO_INCREMENT PRIMARY KEY, temperature VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
 routes_capteurs = glob.glob("/sys/bus/w1/devices/28*/w1_slave")
 horodatage = datetime.now()
 horodatage_strg = horodatage.strftime("%X")
