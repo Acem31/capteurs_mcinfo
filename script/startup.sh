b8f7c1fce3e29b2ebc9e8d02465a3e808db2e45c
Acem31 <aymeric.blot@sfr.fr>
2022-09-17 14:06:05 +0200

    Update startup.sh

--- script/startup.sh
+++ script/startup.sh
@@ -4,9 +4,11 @@ apt install -y install python3-pip
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 pip3 install docker-compose
+pip3 install mysql-connector-python
 usermod -aG docker capteurs
 wget https://raw.githubusercontent.com/Acem31/capteurs_mcinfo/main/docker/Dockerfile
 wget https://raw.githubusercontent.com/Acem31/capteurs_mcinfo/main/docker/docker-compose.yml
 wget https://raw.githubusercontent.com/Acem31/capteurs_mcinfo/main/python/capteurs_temp_test.py
+
 service ssh restart
 reboot
