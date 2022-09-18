e6a8b288f5d9e95b9da81e977a0508260f1831a7
Acem31 <aymeric.blot@sfr.fr>
2022-09-18 11:10:41 +0200

    Update reset.php

--- reset.php
+++ reset.php
@@ -1,13 +1,5 @@
 <?php
-		$mysqli = new mysqli("mysql_db", "root", "couchpass31", "couch_DB");
-    if($mysqli === false){
-      die("Problème de base de données! " . mysqli_connect_error());
-  }
-		$requete = "TRUNCATE TABLE capteur1";
-    $resultat = mysqli_query($mysqli, $requete);
-
-    
-	$mysqli->close();
+	shell_exec("python ./python/reset.py")
 	header('Location: http://www.votresite.com/pageprotegee.php');
   	exit();
 		?>
