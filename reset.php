ffb6b2e2e9a4c3746538ea5f8af6c9f00572babb
Acem31 <aymeric.blot@sfr.fr>
2022-09-18 11:28:49 +0200

    Update reset.php

--- reset.php
+++ reset.php
@@ -1,5 +1,5 @@
 <?php
-	shell_exec("/usr/bin/python /var/www/html/python/reset.py");
+	exec("/usr/bin/python /var/www/html/python/reset.py");
 	header('Location: http://capteurs.local');
   	exit();
 		?>
