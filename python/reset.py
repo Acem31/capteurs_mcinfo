096d37c05d305cdeef7e27cd465b55ef73fda2ad
Acem31 <aymeric.blot@sfr.fr>
2022-09-18 10:16:20 +0200

    Create reset.py

---
+++ python/reset.py
@@ -0,0 +1,45 @@
+import glob
+import mysql.connector
+from datetime import datetime
+
+
+conn = mysql.connector.connect(
+		host='localhost',
+		user='root',
+		password = "couchpass31",
+		database='couch_DB',
+		)
+
+def lire_fichier (emplacement) :
+    fichier = open(emplacement)
+    contenu = fichier.read()
+    fichier.close()
+    return contenu
+
+
+def extraire_temperature (contenu) :
+    seconde_ligne = contenu.split("\n")[1]
+    donnees_temperature = seconde_ligne.split(" ")[9]
+    return float(donnees_temperature[2:]) / 1000
+
+mycursor = conn.cursor()
+mycursor.execute("CREATE TABLE IF NOT EXISTS capteur1 (id INT AUTO_INCREMENT PRIMARY KEY, temperature VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
+routes_capteurs = glob.glob("/sys/bus/w1/devices/28*/w1_slave")
+horodatage = datetime.now()
+horodatage_strg = horodatage.strftime("%X")
+horodatage_day = horodatage.strftime("%x")
+
+mycursor.execute("TRUNCATE TABLE capteur1")
+
+if len(routes_capteurs) > 0 :
+    contenu_fichier = lire_fichier(routes_capteurs[0])
+    temperature = extraire_temperature(contenu_fichier)
+    sql = "INSERT INTO capteur1 (temperature, horodatage, date) VALUES (%s, %s, %s)"
+    val = (temperature, horodatage_strg, horodatage_day)
+    mycursor.execute(sql, val)
+    conn.commit()
+
+else :
+    print("Sonde non détectee. Vérifier le branchement, ou rendez-vous dans la section montrant une solution possible")
+
+
