b968561e5d36a75a60802b0e76e5da71d320b65b
Acem31 <50300072+Acem31@users.noreply.github.com>
2022-09-20 16:05:28 +0200

    Add files via upload

---
+++ python/capteur_hygro_test.py
@@ -0,0 +1,24 @@
+import time
+import board
+import adafruit_dht
+
+dhtDevice = adafruit_dht.DHT11(board.D25)
+
+while True:
+    try:
+        humidity = dhtDevice.humidity
+        print(
+            "Humidity: {}% ".format(
+                humidity
+            )
+        )
+
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(2.0)
