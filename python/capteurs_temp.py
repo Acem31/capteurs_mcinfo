c1c0b1684ebb45361461e8548cc20f4901be8b1a
Acem31 <aymeric.blot@sfr.fr>
2022-09-21 22:25:13 +0200

    Update capteurs_temp.py

--- python/capteurs_temp.py
+++ python/capteurs_temp.py
@@ -28,13 +28,14 @@ mycursor.execute("CREATE TABLE IF NOT EXISTS capteur1 (id INT AUTO_INCREMENT PRI
 mycursor.execute("CREATE TABLE IF NOT EXISTS capteur2 (id INT AUTO_INCREMENT PRIMARY KEY, temperature VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
 mycursor.execute("CREATE TABLE IF NOT EXISTS capteur3 (id INT AUTO_INCREMENT PRIMARY KEY, temperature VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
 mycursor.execute("CREATE TABLE IF NOT EXISTS capteur4 (id INT AUTO_INCREMENT PRIMARY KEY, temperature VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS hygro1 (id INT AUTO_INCREMENT PRIMARY KEY, humidite VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
 routes_capteurs1 = glob.glob("/sys/bus/w1/devices/28-3c01f0957762/w1_slave")
 routes_capteurs2 = glob.glob("/sys/bus/w1/devices/28-3c01f0952eb5/w1_slave")
 routes_capteurs3 = glob.glob("/sys/bus/w1/devices/28-3c01f095702b/w1_slave")
 routes_capteurs4 = glob.glob("/sys/bus/w1/devices/28-3c01f0950ae4/w1_slave")
 horodatage = datetime.now()
-horodatage_strg = horodatage.strftime("%X")
-horodatage_day = horodatage.strftime("%x")
+horodatage_strg = horodatage.strftime("%H:%M")
+horodatage_day = horodatage.strftime("%d-%m")
 
 if len(routes_capteurs1) > 0 :
     contenu_fichier = lire_fichier(routes_capteurs1[0])
@@ -45,7 +46,11 @@ if len(routes_capteurs1) > 0 :
     print ("Date :", horodatage_day ,"\n")
     sql = "INSERT INTO capteur1 (temperature, horodatage, date) VALUES (%s, %s, %s)"
     val = (roundtemp, horodatage_strg, horodatage_day)
+    hum = 100-(roundtemp*2)
     mycursor.execute(sql, val)
+    sql2 = "INSERT INTO hygro1 (humidite, horodatage, date) VALUES (%s, %s, %s)"
+    val2 = (hum, horodatage_strg, horodatage_day)
+    mycursor.execute(sql2, val2)
     conn.commit()
 
 if len(routes_capteurs2) > 0 :
