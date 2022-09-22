ac00eee700ca8aa5c6524263effe1330c0b71df9
Acem31 <aymeric.blot@sfr.fr>
2022-09-22 23:15:53 +0200

    Update archiveshum.php

--- archiveshum.php
+++ archiveshum.php
@@ -19,63 +19,24 @@
       </div>
 		<div class="zone">
         <div class="capt">
-			<h2>Capteur 1</h2>
+			<h2>Hygrométrie 1</h2>
 			<table>
 			<?php
 				$mysqli = new mysqli("mysql_db", "root", "couchpass31", "couch_DB");
     			if($mysqli === false){
       				die("Problème de base de données! " . mysqli_connect_error());
   				}
-				$requete = "SELECT * FROM capteur1 ORDER BY id DESC";
+				$requete = "SELECT * FROM hygro1 ORDER BY id DESC";
 				$resultat = mysqli_query($mysqli, $requete);
 
 				while ($ligne = $resultat->fetch_assoc()) {
-					printf("<tr><td>%s°</td><td>%s</td><td>%s</td></tr>", $ligne["temperature"], $ligne["horodatage"], $ligne["date"]);
+					printf("<tr><td>%s &#37;</td><td>%s</td><td>%s</td></tr>", $ligne["humidite"], $ligne["horodatage"], $ligne["date"]);
 				}
-			?>
+				?>
 			</table>
 		</div>
 			<div class="capt">
-				<h2>Capteur 2</h2>
-				<table>
-			<?php
-				$requete = "SELECT * FROM capteur2 ORDER BY id DESC";
-				$resultat = mysqli_query($mysqli, $requete);
-
-				while ($ligne = $resultat->fetch_assoc()) {
-					printf("<tr><td>%s°</td><td>%s</td><td>%s</td></tr>", $ligne["temperature"], $ligne["horodatage"], $ligne["date"]);
-				}
-			?>
-					</table>
-		</div>
-			<div class="capt">
-				<h2>Capteur 3</h2>
-				<table>
-			<?php
-				$requete = "SELECT * FROM capteur3 ORDER BY id DESC";
-				$resultat = mysqli_query($mysqli, $requete);
-
-				while ($ligne = $resultat->fetch_assoc()) {
-					printf("<tr><td>%s°</td><td>%s</td><td>%s</td></tr>", $ligne["temperature"], $ligne["horodatage"], $ligne["date"]);
-				}
-			?>
-					</table>
-		</div>
-			<div class="capt">
-				<h2>Capteur 4</h2>
-				<table>
-			<?php
-				$requete = "SELECT * FROM capteur4 ORDER BY id DESC";
-				$resultat = mysqli_query($mysqli, $requete);
-
-				while ($ligne = $resultat->fetch_assoc()) {
-					printf("<tr><td>%s°</td><td>%s</td><td>%s</td></tr>", $ligne["temperature"], $ligne["horodatage"], $ligne["date"]);
-				}
-			?>
-					</table>
-		</div>
-			<div class="capt">
-				<h2>Hygrométrie</h2>
+				<h2>Hygrométrie 2</h2>
 				<table>
 			<?php
 				$requete = "SELECT * FROM hygro1 ORDER BY id DESC";
@@ -87,7 +48,32 @@
 				?>
 					</table>
 		</div>
-			
+			<div class="capt">
+				<h2>Hygrométrie 3</h2>
+				<table>
+			<?php
+				$requete = "SELECT * FROM hygro1 ORDER BY id DESC";
+				$resultat = mysqli_query($mysqli, $requete);
+
+				while ($ligne = $resultat->fetch_assoc()) {
+					printf("<tr><td>%s &#37;</td><td>%s</td><td>%s</td></tr>", $ligne["humidite"], $ligne["horodatage"], $ligne["date"]);
+				}
+				?>
+					</table>
+		</div>
+			<div class="capt">
+				<h2>Hygrométrie 4</h2>
+				<table>
+			<?php
+				$requete = "SELECT * FROM hygro1 ORDER BY id DESC";
+				$resultat = mysqli_query($mysqli, $requete);
+
+				while ($ligne = $resultat->fetch_assoc()) {
+					printf("<tr><td>%s &#37;</td><td>%s</td><td>%s</td></tr>", $ligne["humidite"], $ligne["horodatage"], $ligne["date"]);
+				}
+				?>
+					</table>
+		</div>
 		</div>
 	<?php
 	$mysqli->close();
