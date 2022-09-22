7aad2b094ee57f6fd41f7be586a80b5b9c813c89
Acem31 <aymeric.blot@sfr.fr>
2022-09-22 22:58:57 +0200

    Update capteur_hygro_test.py

--- python/capteur_hygro_test.py
+++ python/capteur_hygro_test.py
@@ -13,7 +13,7 @@ conn = mysql.connector.connect(
 
 mycursor = conn.cursor()
 
-humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 25)
+humidity1, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 25)
 
 mycursor.execute("CREATE TABLE IF NOT EXISTS hygro1 (id INT AUTO_INCREMENT PRIMARY KEY, humidite VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
 mycursor.execute("CREATE TABLE IF NOT EXISTS hygro2 (id INT AUTO_INCREMENT PRIMARY KEY, humidite VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
@@ -23,10 +23,34 @@ horodatage = datetime.now()
 horodatage_strg = horodatage.strftime("%H:%M")
 horodatage_day = horodatage.strftime("%d-%m")
 
-if humidity is not None:
-   roundhum = round(humidity, 2)
+if humidity1 is not None:
+   roundhum = round(humidity1, 2)
     print("Humidity=", roundhum,"%")
-else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
-    
+    sql = "INSERT INTO hygro1 (humidite, horodatage, date) VALUES (%s, %s, %s)"
+    val = (roundhum, horodatage_strg, horodatage_day)
+    mycursor.execute(sql, val)
+    conn.commit()
+
+if humidity1 is not None:
+   roundhum = round(humidity1, 2)
+    print("Humidity=", roundhum,"%")
+    sql = "INSERT INTO hygro2 (humidite, horodatage, date) VALUES (%s, %s, %s)"
+    val = (roundhum, horodatage_strg, horodatage_day)
+    mycursor.execute(sql, val)
+    conn.commit()
+
+if humidity1 is not None:
+   roundhum = round(humidity1, 2)
+    print("Humidity=", roundhum,"%")
+    sql = "INSERT INTO hygro3 (humidite, horodatage, date) VALUES (%s, %s, %s)"
+    val = (roundhum, horodatage_strg, horodatage_day)
+    mycursor.execute(sql, val)
+    conn.commit()
+
+if humidity1 is not None:
+   roundhum = round(humidity1, 2)
+    print("Humidity=", roundhum,"%")
+    sql = "INSERT INTO hygro4 (humidite, horodatage, date) VALUES (%s, %s, %s)"
+    val = (roundhum, horodatage_strg, horodatage_day)
+    mycursor.execute(sql, val)
+    conn.commit()
