6fa935bb13714fbb40d38eb05630b220b1246be4
Acem31 <aymeric.blot@sfr.fr>
2022-09-22 22:55:15 +0200

    Update capteur_hygro_test.py

--- python/capteur_hygro_test.py
+++ python/capteur_hygro_test.py
@@ -1,11 +1,31 @@
 import sys
 import Adafruit_DHT
+import glob
+import mysql.connector
+from datetime import datetime
+
+conn = mysql.connector.connect(
+		host='localhost',
+		user='root',
+		password = "couchpass31",
+		database='couch_DB',
+		)
+
+mycursor = conn.cursor()
 
 humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 25)
 
+mycursor.execute("CREATE TABLE IF NOT EXISTS hygro1 (id INT AUTO_INCREMENT PRIMARY KEY, humidite VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS hygro2 (id INT AUTO_INCREMENT PRIMARY KEY, humidite VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS hygro3 (id INT AUTO_INCREMENT PRIMARY KEY, humidite VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS hygro4 (id INT AUTO_INCREMENT PRIMARY KEY, humidite VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
+horodatage = datetime.now()
+horodatage_strg = horodatage.strftime("%H:%M")
+horodatage_day = horodatage.strftime("%d-%m")
+
 if humidity is not None:
    roundhum = round(humidity, 2)
-    print("Humidity=", humidity,"%")
+    print("Humidity=", roundhum,"%")
 else:
     print('Failed to get reading. Try again!')
     sys.exit(1)
