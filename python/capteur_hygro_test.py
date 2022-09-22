8b87244b583d1aa724653adf03d79d7f3113d6d8
Acem31 <aymeric.blot@sfr.fr>
2022-09-22 22:49:29 +0200

    Update capteur_hygro_test.py

--- python/capteur_hygro_test.py
+++ python/capteur_hygro_test.py
@@ -4,6 +4,7 @@ import Adafruit_DHT
 humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 25)
 
 if humidity is not None:
+   roundhum = round(humidity, 2)
     print("Humidity=", humidity,"%")
 else:
     print('Failed to get reading. Try again!')
