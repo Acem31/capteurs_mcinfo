4a3d2d272b89482ccb742a370f6f4333ec51922a
Acem31 <aymeric.blot@sfr.fr>
2022-09-24 09:52:07 +0200

    Update exporter.php

--- exporter.php
+++ exporter.php
@@ -1,32 +1,34 @@
 <?php
-query($query);
-$items = array();
-
-
-while( $row = $result->fetch_assoc() ) {
-$items[] = $row;
+ 
+use PhpOfficePhpSpreadsheetSpreadsheet;
+use PhpOfficePhpSpreadsheetWriterXlsx;
+ 
+$spreadsheet = new Spreadsheet();
+$Excel_writer = new Xlsx($spreadsheet);
+ 
+$spreadsheet->setActiveSheetIndex(0);
+$activeSheet = $spreadsheet->getActiveSheet();
+ 
+$activeSheet->setCellValue('A1', 'Product Name');
+$activeSheet->setCellValue('B1', 'Product SKU');
+$activeSheet->setCellValue('C1', 'Product Price');
+ 
+$query = $mysqli->query("SELECT * FROM products ORDER BY id DESC");
+ 
+if($query->num_rows > 0) {
+    $i = 2;
+    while($row = $query->fetch_assoc()) {
+        $activeSheet->setCellValue('A'.$i, $row['product_name']);
+        $activeSheet->setCellValue('B'.$i, $row['product_sku']);
+        $activeSheet->setCellValue('C'.$i, $row['product_price']);
+        $i++;
+    }
 }
-
-if(isset($_POST["export"])) {
-//Define the filename with current date
-$fileName = "itemdata-".date('d-m-Y').".xls";
-
-
+ 
+$filename = 'products.xlsx';
+ 
 header('Content-Type: application/vnd.ms-excel');
-header('Content-Disposition: attachment; filename='.$fileName);
-
-$heading = false;
-
-if(!empty($items)) {
-foreach($items as $item) {
-if(!$heading) {
-echo implode("\t", array_keys($item)) . "\n";
-$heading = true;
-}
-echo implode("\t", array_values($item)) . "\n";
-}
-}
-exit();
-}
-
+header('Content-Disposition: attachment;filename='. $filename);
+header('Cache-Control: max-age=0');
+$Excel_writer->save('php://output');
 ?>
