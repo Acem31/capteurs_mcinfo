0b91cb4ec904b021b84cfbd89d73c834b273480e
Acem31 <aymeric.blot@sfr.fr>
2022-09-24 11:01:24 +0200

    Update reset.php

--- reset.php
+++ reset.php
@@ -2,7 +2,7 @@
 	$commande = escapeshellcmd ("/usr/bin/python3.9 /var/www/html/python/reset.py");
 	$output = shell_exec($commande);
 	echo $output;
-	if(function_exists('exec')) {
+	if(function_exists('shell_exec')) {
     echo "exec is enabled";
 	}
 	else header('Location: http://capteurs.local');
