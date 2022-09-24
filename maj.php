ad0f9d3865cdeb93ebb41185487d8ade2a31228f
Acem31 <aymeric.blot@sfr.fr>
2022-09-24 11:09:17 +0200

    Update maj.php

--- maj.php
+++ maj.php
@@ -1,6 +1,7 @@
 <?php
-	$commande = escapeshellcmd ("/usr/bin/python3.9 /var/www/html/python/capteurs_temp.py");
-	$output = shell_exec($commande);
+	/*$commande = escapeshellcmd ("/usr/bin/python3.9 /var/www/html/python/capteurs_temp.py");
+	$output = shell_exec($commande);*/
+	$output = shell_exec('/usr/bin/python3.9 /var/www/html/python/capteurs_temp.py');
 	echo $output;
 	if(function_exists('escapeshellcmd')) {
     echo "escapeshellcmd is enabled";
