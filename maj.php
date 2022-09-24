3abf74fdd546fd474549ab327e4084ed0febb7f4
Acem31 <aymeric.blot@sfr.fr>
2022-09-24 11:41:32 +0200

    Update maj.php

--- maj.php
+++ maj.php
@@ -2,6 +2,8 @@
 	/*$commande = escapeshellcmd ("/usr/bin/python3.9 /var/www/html/python/capteurs_temp.py");
 	$output = shell_exec($commande);*/
 	$output = shell_exec('/var/www/html/python/capteurs_temp.py');
-	echo $output;header('Location: http://capteurs.local');
+	echo $output;
+	$info = shell_exec('echo $USER');
+	/*header('Location: http://capteurs.local');*/
   	exit();
 		?>
