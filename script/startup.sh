c9b5a986f82bbb4d55bf654c6e521f30c86d996b
Acem31 <aymeric.blot@sfr.fr>
2022-09-24 16:49:06 +0200

    Update startup.sh

--- script/startup.sh
+++ script/startup.sh
@@ -3,8 +3,6 @@ apt update
 apt install -y install python3-pip
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-apt -y install ecryptfs-utils
-modprobe ecryptfs
 pip3 install docker-compose
 pip3 install mysql-connector-python
 usermod -aG docker capteurs
@@ -15,5 +13,7 @@ wget https://raw.githubusercontent.com/Acem31/capteurs_mcinfo/main/python/capteu
 docker-compose up -d
 python capteurs_temp_test.py
 python capteur_hygro_test.py
+docker exec -ti appache_www chown root.gpio /dev/gpiomem
+docker exec -ti appache_www chown chmod g+rw /dev/gpiomem
 service ssh restart
 reboot
