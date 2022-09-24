45c624c5f400c29edea3224d3e97f14dfe5ea8a9
Acem31 <aymeric.blot@sfr.fr>
2022-09-24 22:40:06 +0200

    Update maj.php

--- maj.php
+++ maj.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <?php
 	$commande = escapeshellcmd ("/usr/bin/python3.9 /var/www/html/python/capteurs_temp.py");
 	$output = shell_exec($commande);
@@ -5,3 +6,27 @@
 	header('Location: http://capteurs.local');
   	exit();
 		?>
+<html dir="ltr" lang="fr">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Chargement...</title>
+    <link rel="stylesheet" href="capteurs.css">
+	<link rel="icon" href="favicon.ico" />
+</style><meta name="author" content="BLOT Aymeric">
+<meta http-equiv="refresh" content="3;URL='http://capteurs.local'">
+	</head>
+<body>
+	<div id='chargement'>
+	<div class="loader">Loading...</div>
+	<p>Mise à jour des capteurs...</p>
+	</div>
+	<footer>
+		  <center>
+			  <p>Copyright 
+    <?php echo date("Y"); ?> 
+<a href="https://www.mc-informatique.fr/" title="Site internet de MC Informatique à l'Union (31)">mc-informatique.fr</a>. Tous droits réservés.
+			  </p>
+		  </center>
+	  </footer>
+</body>
+</html>
