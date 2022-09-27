c5b1cb555dc78ec3fb6bbb4728af5ea2b102603c
Acem31 <aymeric.blot@sfr.fr>
2022-09-27 17:40:54 +0200

    Update maj.php

--- maj.php
+++ maj.php
@@ -5,7 +5,6 @@
     <title>Chargement...</title>
     <link rel="stylesheet" href="capteurs.css">
 	<link rel="icon" href="favicon.ico" />
-</style><meta name="author" content="BLOT Aymeric">
 	</head>
 <body>
 		<div id='chargement'>
@@ -13,12 +12,6 @@
 	<p>Mise à jour des capteurs...</p>
 	</div>
 	<footer>
-		  <center>
-			  <p>Copyright 
-    <?php echo date("Y"); ?> 
-<a href="https://www.mc-informatique.fr/" title="Site internet de MC Informatique à l'Union (31)">mc-informatique.fr</a>. Tous droits réservés.
-			  </p>
-		  </center>
 		<script>
         var timer = setTimeout(function() {
             window.location='./index.php'
