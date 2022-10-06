0cf8ebec41c1babade65e1478fef23a083906aca
Acem31 <50300072+Acem31@users.noreply.github.com>
2022-10-06 18:09:42 +0200

    Update reset.py

--- python/reset.py
+++ python/reset.py
@@ -25,9 +25,8 @@ def extraire_temperature (contenu) :
 
 mycursor = conn.cursor()
 
-humidity1, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 25)
+humidity1, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 04)
 humidity2, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 25)
-humidity3, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 25)
 
 mycursor.execute("CREATE TABLE IF NOT EXISTS capteur1 (id INT AUTO_INCREMENT PRIMARY KEY, temperature VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
 mycursor.execute("CREATE TABLE IF NOT EXISTS capteur2 (id INT AUTO_INCREMENT PRIMARY KEY, temperature VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
