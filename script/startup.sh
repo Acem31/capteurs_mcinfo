3c931acde4d84a68dcd628b76d5841ca52132b6b
Acem31 <aymeric.blot@sfr.fr>
2022-10-12 13:09:10 +0200

    Update startup.sh

--- script/startup.sh
+++ script/startup.sh
@@ -3,6 +3,7 @@ apt update
 apt install -y install docker docker-compose python3-pip libgpiod2 python-is-python3
 pip3 install mysql-connector-python
 pip3 install Adafruit-DHT
+pip3 install adafruit-circuitpython-dht
 pip3 install mysql-connector-python
 usermod -aG docker capteurs
 wget https://raw.githubusercontent.com/Acem31/capteurs_mcinfo/main/docker/Dockerfile
