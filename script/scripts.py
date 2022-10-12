163e8db3e7b26767bcd70bb695f6525bcbd9d1b4
Acem31 <aymeric.blot@sfr.fr>
2022-10-12 12:56:13 +0200

    Update scripts.py

--- script/scripts.py
+++ script/scripts.py
@@ -1,3 +1,8 @@
 import subprocess
+import os
 
-subprocess.run(["/home/capteurs/droits.sh"])
+cmd1 = 'docker exec -ti appache_www chown root.gpio /dev/gpiomem'
+cmd2 = 'docker exec -ti appache_www chmod g+rw /dev/gpiomem'
+
+os.system(cmd1)
+os.system(cmd2)
