f2f875b38b2f1a6b34e34b4aa1c875351ec52cf5
Acem31 <aymeric.blot@sfr.fr>
2022-10-12 19:22:51 +0200

    Update scripts.py

--- script/scripts.py
+++ script/scripts.py
@@ -3,6 +3,8 @@ import os
 
 cmd1 = 'docker exec -ti appache_www chown root.gpio /dev/gpiomem'
 cmd2 = 'docker exec -ti appache_www chmod g+rw /dev/gpiomem'
+cmd3 = 'docker exec -ti appache_www chown root.gpio /sys'
+cmd4 = 'docker exec -ti appache_www chmod g+rw /sys'
 
 os.system(cmd1)
 os.system(cmd2)
