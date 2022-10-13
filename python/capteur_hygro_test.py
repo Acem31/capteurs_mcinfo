5e6a97b23b8281b6915db5f3c9c40b74683ae1a9
Acem31 <50300072+Acem31@users.noreply.github.com>
2022-10-13 09:24:06 +0200

    Update capteur_hygro_test.py

--- python/capteur_hygro_test.py
+++ python/capteur_hygro_test.py
@@ -5,6 +5,7 @@ import board
 import mysql.connector
 from datetime import datetime
 import time
+import RPi.GPIO as GPIO  
 
 
 conn = mysql.connector.connect(
@@ -16,15 +17,17 @@ conn = mysql.connector.connect(
 
 mycursor = conn.cursor()
 
+dht1 = adafruit_dht.DHT22(19)
+humidity1 = dht1.humidity
+
+dht2 = adafruit_dht.DHT22(6)
+humidity2 = dht2.humidity
 
 mycursor.execute("CREATE TABLE IF NOT EXISTS hygro1 (id INT AUTO_INCREMENT PRIMARY KEY, humidite VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
 mycursor.execute("CREATE TABLE IF NOT EXISTS hygro2 (id INT AUTO_INCREMENT PRIMARY KEY, humidite VARCHAR(255), horodatage VARCHAR(255), date VARCHAR(255))")
 horodatage = datetime.now()
 horodatage_strg = horodatage.strftime("%H:%M")
 horodatage_day = horodatage.strftime("%d-%m")
-
-dht1 = adafruit_dht.DHT22(19)
-humidity1 = dht1.humidity
 
 if humidity1 == None :
       print ("Humidité :", 0, "%")
@@ -46,8 +49,6 @@ else:
       mycursor.execute(sql, val)
       conn.commit()
 
-dht2 = adafruit_dht.DHT22(6)
-humidity2 = dht2.humidity
 
 if humidity2 == None :
       print ("Humidité :", 0, "%")
@@ -68,3 +69,5 @@ else:
       val = (0, horodatage_strg, horodatage_day)
       mycursor.execute(sql, val)
       conn.commit()
+	
+GPIO.cleanup()
